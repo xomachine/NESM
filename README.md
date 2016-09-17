8f57b472e7f3e0ca05845bb50d22ad36558e7db1
xomachine <xomachiner@gmail.com>
2016-09-17 19:55:06 +0300

    Added a little documentation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NESM
 NESM stands for Nim's Easy Serialization Macro. The macro that allowing generation of serialization functions by one line of code!
+
+Documentation can be found ![nesm.html](here).
--- nesm.nim
+++ nesm.nim
@@ -4,6 +4,78 @@ from strutils import `%`, parseInt
 from tables import Table, initTable, contains, `[]`, `[]=`
 from strscans import scanf
 
+## **NESM** is a tool that generates serialization and deserialization code for
+## given object. This library provides a macro called `serializable` inside which
+## the object description should be placed.
+##
+## For example:
+##
+## .. code-block:: nim
+##   serializable:
+##     type Ball = object
+##       weight: float32
+##       diameter: int32
+##       isHollow: bool
+##
+## In this example you could notice that `int32` and `float32` declarations are used
+## instead of just `int` and `float`. It is necessary to avoid ambiguity in
+## declarations on different platforms.
+##
+## The code in example will be transformed into the following code:
+##
+## .. code-block:: nim
+##    type
+##      Ball = object
+##        weight: float32
+##        diameter: int32
+##        isHollow: bool
+##  
+##    proc size(q: typedesc[Ball]): int =
+##      9
+##  
+##    proc serialize(obj: Ball): array[0 .. 8, byte] =
+##      copyMem(result[0].unsafeAddr, obj.weight.unsafeAddr, 4)
+##      copyMem(result[4].unsafeAddr, obj.diameter.unsafeAddr, 4)
+##      copyMem(result[8].unsafeAddr, obj.isHollow.unsafeAddr, 1)
+##  
+##    proc deserialize(q: typedesc[Ball]; data: seq[byte]): Ball =
+##      assert(data.len() >= 9, "Given sequence should contain at least 9 bytes!")
+##      copyMem(result.weight.unsafeAddr, data[0].unsafeAddr, 4)
+##      copyMem(result.diameter.unsafeAddr, data[4].unsafeAddr, 4)
+##      copyMem(result.isHollow.unsafeAddr, data[8].unsafeAddr, 1)
+##
+## As you can see from the code above, each memory region of variable is copied
+## to the resulting array back-to-back. This approach achieves both of smallest
+## serialized object size and independence from the compilator specific object
+## representation.
+##
+## At the moment the following types of object are supported:
+## - Aliases from basic types or previously defined in this block:
+##     .. code-block:: nim
+##       type MyInt = int16
+##
+## - Distinct types from basic types or previously defined in this block:
+##     .. code-block:: nim
+##       type MyInt = distinct int16
+##
+## - Tuples, objects and arrays:
+##     .. code-block:: nim
+##       type MyTuple = tuple[a: float64, b: int64]
+##
+## - Nested tuples and objects if they are previously defined
+##   in the same `serializable` block:
+##     .. code-block:: nim
+##       type MyNestedTuple = tuple[a: float64, b: int64]
+##       type MyTuple = tuple[a: float32, b: MyNestedTuple]
+##
+## - Nested arrays of basic types:
+##     .. code-block:: nim
+##       type Matrix = array[0..4, array[0..4, int32]]
+##
+## Arrays of tuples or objects defined before are semantically correct and
+## don't produce error but can lead to undefined behaviour.
+##
+
 when defined(js):
   error("Non C-like targets non supported yet.")
 
