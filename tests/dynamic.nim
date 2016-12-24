982b3ad0fc4cda76333a8144480dc857e318c978
xomachine <xomachiner@gmail.com>
2016-12-24 22:07:40 +0300

    Little fixes of the random function call

--- tests/dynamic.nim
+++ tests/dynamic.nim
@@ -8,13 +8,12 @@ type
     reader: proc (c: Natural): seq[byte]
     writer: proc (s: pointer, c: Natural)
 
-const ascii_range = ord('z') - ord('A')
 
 proc get_random_reader_n_writer(): RnW =
   var read_data = newSeq[byte]()
   var index = 0
   result.reader = proc(c:Natural): seq[byte] =
-    result = newSeqWith(c, byte(ord('A')+random(ascii_range)))
+    result = newSeqWith(c, byte(random(ord('A')..ord('Z'))))
     read_data &= result
 
   result.writer = proc(s:pointer, c:Natural) =
@@ -50,7 +49,7 @@ suite "Dynamic structure tests":
         MySeqObj = object
           a: seq[int32]
     var obj: MySeqObj
-    obj.a = newSeqWith(1+random(50), int32(random(100)))
+    obj.a = newSeqWith(random(1..50), int32(random(100)))
     let rnw = get_reader_n_writer()
     obj.serialize(rnw.writer)
     let another_obj = MySeqObj.deserialize(rnw.reader)
@@ -64,9 +63,9 @@ suite "Dynamic structure tests":
         MyObj = object
           a: seq[seq[int32]]
     var obj: MyObj
-    obj.a = newSeq[seq[int32]](1+random(10))
+    obj.a = newSeq[seq[int32]](random(1..11))
     for i in 0..<obj.a.len:
-      obj.a[i] = newSeq[int32](1+random(10))
+      obj.a[i] = newSeq[int32](random(1..11))
       for j in 0..<obj.a[i].len:
         obj.a[i][j] = random(100).int32
     let rnw = get_reader_n_writer()
