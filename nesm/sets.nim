e2ad093f68897640d63eeb052d87c78e1925cf7a
xomachine <xomachiner@gmail.com>
2017-05-08 23:46:08 +0300

    Fixed sets implementation to pass enum test

--- nesm/sets.nim
+++ nesm/sets.nim
@@ -1,18 +1,23 @@
 import macros
+from strutils import `%`
+from tables import contains, `[]`
 from nesm.typesinfo import TypeChunk, Context
 from nesm.typesinfo import isBasic, estimateBasicSize
-from nesm.enums import estimateEnumSize
 from nesm.basics import genBasic
 
 proc genSet*(context: Context, declaration: NimNode): TypeChunk {.compileTime.}
 
 proc genSet(context: Context, declaration: NimNode): TypeChunk =
   declaration.expectMinLen(2)
-  let undertype = declaration[1]
-  let size =
-    if ($undertype).isBasic():
-      estimateBasicSize($undertype)
-    else:
-      estimateEnumSize(getType(undertype).last.last[2])
-  context.genBasic(1 shl (size * 8 - 3))
+  let undertype = $declaration[1]
+  if undertype.isBasic():
+    let size = estimateBasicSize(undertype)
+    result = context.genBasic(1 shl (size * 8 - 3))
+  elif undertype in context.declared:
+    let enumtype = context.declared[undertype]
+    if enumtype.nodekind != nnkEnumTy:
+      error("The type '$1' neither of enum or basic type!" % undertype)
+    result = context.genBasic(int(enumtype.maxcount div 8) + 1)
+  else:
+    error("Impossible type under the set: " & declaration.repr)
 
