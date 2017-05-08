75771b296c22bf1e0eafbcef3adc11157cd904f1
xomachine <xomachiner@gmail.com>
2017-05-08 23:42:22 +0300

    Improved set[enum] test to catch sizing errors

--- tests/settest.nim
+++ tests/settest.nim
@@ -9,6 +9,7 @@ suite "Sets":
     serializable:
       static:
         type CharSet = set[char]
+    require(CharSet.sizeof == size(CharSet))
     let rnw = get_random_reader_n_writer()
     let o = CharSet.deserialize(rnw)
     rnw.setPosition(0)
@@ -19,7 +20,9 @@ suite "Sets":
       static:
         type TEnum = enum
           A, B, C
+          D = 1024
         type ESet = set[TEnum]
+    require(ESet.sizeof == size(ESet))
     let rnw = get_random_reader_n_writer()
     let o = ESet.deserialize(rnw)
     rnw.setPosition(0)
