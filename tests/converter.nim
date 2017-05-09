1d3c4f694b941cfb815a3ee0fcf393bab45017d9
xomachine <xomachiner@gmail.com>
2017-05-09 11:28:20 +0300

    Added two more toSerializable tests

--- tests/converter.nim
+++ tests/converter.nim
@@ -1,6 +1,7 @@
-from nesm import toSerializable
+from nesm import toSerializable, serializable
 from streams import setPosition
 from basic2d import Point2d
+from endians import bigEndian32
 import unittest
 import helpers.rnw
 
@@ -23,4 +24,19 @@ suite "Converter tests":
     let pnt = Point2d.deserialize(rnw)
     rnw.setPosition(0)
     pnt.serialize(rnw)
+  test "Context restoration":
+    # It's just a compilation test
+    toSerializable(Point2d, dynamic: false)
+    serializable:
+      type A = object
+        a: string
+  test "Endians at aliases":
+    type AInt = int32
+    toSerializable(AInt, endian: bigEndian)
+    let a = random(1..1000).AInt
+    let rnw = get_reader_n_writer()
+    a.serialize(rnw)
+    var b = 0.AInt
+    swapEndian32(b.addr, rnw.buffer[0].addr)
+    check(b == a)
 
