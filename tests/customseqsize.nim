c9c88cf71ffdb6c18770e829f1d12f87d06a31fb
xomachine <xomachiner@gmail.com>
2017-05-23 17:40:04 +0300

    Added test for the custom seq size feature

---
+++ tests/customseqsize.nim
@@ -0,0 +1,52 @@
+from nesm import serializable
+from streams import setPosition
+import unittest
+import helpers.rnw
+
+{.hint[XDeclaredButNotUsed]:off.}
+
+suite "Custom periodic size":
+  test "Seq":
+    serializable:
+      type
+        MySeq = object
+          size: int32
+          a: int32
+          data: seq[int32] {size: size}
+ 
+    let rnw = get_reader_n_writer()
+    var o: MySeq
+    o.data = random_seq_with(random(20000).int32)
+    o.size = o.data.len.int32
+    o.a = random(20000).int32
+    o.serialize(rnw)
+    rnw.setPosition(0)
+    let dso = MySeq.deserialize(rnw)
+    check(o.a == dso.a)
+    require(o.size == dso.size)
+    require(o.data.len == dso.data.len)
+    for i in 0..<o.size:
+      check(o.data[i] == dso.data[i])
+
+  test "strings":
+    serializable:
+      type
+        MyString = object
+          size: int32
+          a: int32
+          data: string {size: size}
+ 
+    let rnw = get_reader_n_writer()
+    var o: MyString
+    o.data = get_random_string()
+    o.size = o.data.len.int32
+    o.a = random(20000).int32
+    o.serialize(rnw)
+    rnw.setPosition(0)
+    let dso = MyString.deserialize(rnw)
+    check(o.a == dso.a)
+    require(o.size == dso.size)
+    require(o.data.len == dso.data.len)
+    for i in 0..<o.size:
+      check(o.data[i] == dso.data[i])
+
