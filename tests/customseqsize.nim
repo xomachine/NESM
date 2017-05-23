6d8154bbb351ec8dcdbf8a09bd52c1615ef56753
xomachine <xomachiner@gmail.com>
2017-05-23 20:18:01 +0300

    Added more complex tests for custom seq size feature

--- tests/customseqsize.nim
+++ tests/customseqsize.nim
@@ -52,3 +52,51 @@ suite "Custom periodic size":
     for i in 0..<o.size:
       check(o.data[i] == dso.data[i])
 
+  test "Seq of strings":
+    serializable:
+      type MySeqStr = object
+        s: int32
+        a: int32
+        data: seq[string] {size: s}
+
+    let rnw = get_reader_n_writer()
+    var o: MySeqStr
+    o.data = random_seq_with(get_random_string())
+    o.s = o.data.len.int32
+    o.a = random(20000).int32
+    o.serialize(rnw)
+    rnw.setPosition(0)
+    let dso = MySeqStr.deserialize(rnw)
+    require(dso.size() == o.size())
+    require(dso.data.len == o.data.len)
+    require(dso.s == o.s)
+    check(dso.a == o.a)
+    for i in 0..<o.size:
+      check(o.data[i] == dso.data[i])
+
+  test "Matrix":
+    serializable:
+      type Matrix = object
+        lines: int32
+        columns: int32
+        data: seq[seq[int32]] {size: lines, size: colomns}
+
+    let rnw = get_reader_n_writer()
+    var o: Matrix
+    o.columns = random(1..20).int32
+    proc get_line(): seq[int32] =
+      result = newSeq[int32](o.columns)
+      for i in 0..<o.columns:
+        result[i] = random(20000).int32
+    o.data = random_seq_with(get_line())
+    o.lines = o.data.len.int32
+    o.serialize(rnw)
+    rnw.setPosition(0)
+    let dso = Matrix.deserialize(rnw)
+    require(o.size() == dso.size())
+    require(o.data.len == dso.data.len)
+    require(o.lines == dso.lines)
+    require(o.colomns == dso.colomns)
+    for i in 0..<o.lines:
+      for j in 0..<o.colomns:
+        check(o.data[i][j] == dso.data[i][j])
